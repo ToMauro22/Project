e370d115e7d940ef33cd006a98aba19673e937bc
ToMauro22 <mmm0109@mix.wvu.edu>
2023-04-28 10:03:06 -0400

    Project

--- ManateeData/manateedata.R
+++ ManateeData/manateedata.R
@@ -2,3 +2,8 @@ setwd('C:\\Users\\migma\\OneDrive\\Desktop\\Evolution\\Project\\ManateeData')
 Data1<-read.csv("ManateeData.csv")
 Adults <- Data1[which(Data1$Developmental.Stage=="adult"),]
 boxplot(Adults$Toughness~Adults$Orientation)
+t.test(Adults$Toughness)
+boxplot(Adults$Yield.Strength~Adults$Orientation)
+t.test(Adults$Yield.Strength)
+boxplot(Adults$Stiffness~Adults$Orientation)
+t.test(Adults$Stiffness)
